74d4de19643eaf52f55acfa03161dbcdd369daa5
rdominguez5 <rdominguez.canas@gmail.com>
2022-06-13 23:27:10 -0500

    fix get_features

--- spotify-recommendations/main.py
+++ spotify-recommendations/main.py
@@ -1,6 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import credentials
+from sklearn.cluster import KMeans
+import acct1_credentials
+import acct2_credentials
 import pandas as pd
 import re
 
@@ -44,27 +46,50 @@ def get_genre_counts(df):
     return liked_genres_df
 
 def get_features(df, genre_list):
-    for genre in genre_list:
-        df[genre] = 1 if genre in df['genres'] else 0
+    for genre in genre_list.index:
+        df[genre] = df.apply(lambda x: 1 if genre in x['genres'] else 0, axis = 1)
 
     return df
+
+def calculate_clusters(df):
+    """
+    Implements K means on frequencies dataframe
+    """      
+    features = df.iloc[:,7:] 
+    kmeans = KMeans(6) 
+
+    #print(features)
+
+    identified_clusters = kmeans.fit_predict(features)
+
+    df_clusters = df.copy()
+    df_clusters['Cluster'] = identified_clusters 
+    df_clusters = df_clusters[['name', 'Cluster']]
+
+    #df.set_index('index', inplace=True) # Set index
+
+    return (df, df_clusters, features)
 
 
 def main():
 
     scope = "user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=credentials.client_ID, client_secret= credentials.client_SECRET, redirect_uri=credentials.redirect_url, scope=scope))
-
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=acct1_credentials.client_ID, client_secret= acct1_credentials.client_SECRET, redirect_uri=acct1_credentials.redirect_url, scope=scope))
+    print("success!")
     liked_tracks_df = get_all_saved_tracks(sp)
-    liked_tracks_df['genres'] = liked_tracks_df['artist_uri'].apply(get_genres, sp = sp)
+    #liked_tracks_df['genres'] = liked_tracks_df['artist_uri'].apply(get_genres, sp = sp)
 
-    liked_artists_df = liked_tracks_df.groupby(['artist_name'])['artist_name'].count()
+    #liked_artists_df = liked_tracks_df.groupby(['artist_name'])['artist_name'].count()
 
-    liked_genres_df = get_genre_counts(liked_tracks_df)
+    #liked_genres_df = get_genre_counts(liked_tracks_df)
 
-    features = get_features(liked_tracks_df, liked_genres_df)
+    #features = get_features(liked_tracks_df, liked_genres_df)
 
-    print(features)
+    #features.to_csv("test.csv", mode='w+')
+
+    #df_clusters = calculate_clusters(features)[1]
+
+    print(liked_tracks_df)
 
 
 if __name__=="__main__":
