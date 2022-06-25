7c07e8ca91b3e7c80e89700e98c3c7bcaa210852
rdominguez5 <rdominguez.canas@gmail.com>
2022-06-25 01:48:37 -0500

    extract add_match_and_supergenre from percentage function

--- spotify-recommendations/main.py
+++ spotify-recommendations/main.py
@@ -89,6 +89,12 @@ def get_match(genres, supergenres, which):
     
     return supergenre if which == 'supergenre' else is_match
 
+def add_match_and_supergenre(df, supergenres):
+    df['is_match'] = df['genres'].apply(get_match, supergenres = supergenres, which = 'is_match')
+    df['supergenre'] = df['genres'].apply(get_match, supergenres = supergenres, which = 'supergenre')
+    
+    return df
+
 def get_match_percentage(df, supergenres):
     df['is_match'] = df['genres'].apply(get_match, supergenres = supergenres, which = 'is_match')
     df['supergenre'] = df['genres'].apply(get_match, supergenres = supergenres, which = 'supergenre')
